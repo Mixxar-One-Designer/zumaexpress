3aa3ffc892b5e9488f2d712e271b2933e1751dfe
Mixxar-One-Designer <muazzammixxa32@gmail.com>
2022-05-04 14:27:48 -0700

    co2

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
-import { Typography } from '@mui/material';
-import Head from 'next/head';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>zumaexpress</title>
-        <meta
-          name="description"
-          content="The Biggest African Online Ecommerce Store"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Typography component="h1" variant="h1">
-        Zuma-Express
-      </Typography>
-    </div>
-  );
+  return <Layout>List Products</Layout>;
 }
